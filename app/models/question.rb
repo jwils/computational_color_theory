3c636ccb9adbca34e50dc4d3ab5302c6f2618860
Josh Wilson <jwils@seas.upenn.edu>
2013-02-03 22:38:56 -0500

    Added taggable to questions

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,6 +3,7 @@ class Question < ActiveRecord::Base
   belongs_to :img2, :class_name => Image, :foreign_key => :img2_id
   has_and_belongs_to_many :quizzes
   has_many :answers
+  acts_as_taggable
 
 
   def self.get_random_questions(num_questions)
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-
+#### Initialize Sanity check ####
 Image.create(:id => 1, :image_name => '1fdb4a1f7f_Arial_hsl0-0-15_24pt_hsl0-0-100.png')
 Image.create(:id => 2, :image_name => '1fdb4a1f7f_Arial_hsl0-0-15_24pt_hsl0-0-0.png')
 Image.create(:id => 3, :image_name => '1fdb4a1f7f_Arial_hsl0-0-40_24pt_hsl0-0-33.png')
@@ -19,6 +19,6 @@ Image.create(:id => 8, :image_name => '1fdb4a1f7f_Arial_hsl0-0-90_24pt_hsl0-0-10
 
 Image.order_by(:id).each do |image1|
   Image.order_by(:id).each do |image2|
-    Question.create(:img1 => image1, :img2 => image2)
+    Question.create(:img1 => image1, :img2 => image2, :tag_list => "sanity check")
   end
 end
