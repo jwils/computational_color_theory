47c89bd7111e1d3da38b5614b7078a3e5a124ade
Josh Wilson <jwils@seas.upenn.edu>
2013-03-07 02:38:24 -0800

    exp query

--- app/models/experiment.rb
+++ app/models/experiment.rb
@@ -1,6 +1,7 @@
 class Experiment < ActiveRecord::Base
   has_and_belongs_to_many :questions
-  has_many :images, :through => :questions, :source => [:img1, :img2], :uniq => true
+  has_many :images, :through => :questions, :uniq => true,
+           :conditions => ['img1 = #{ID} or img2 = #{ID}']
   accepts_nested_attributes_for :images
   has_many :turkee_tasks, :class_name => 'Turkee::TurkeeTask'
   has_many :responses
