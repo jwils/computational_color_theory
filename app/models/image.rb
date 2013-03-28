73d9b6e94cc6cc4168306674bbc341652dc4490e
Josh Wilson <jwils@seas.upenn.edu>
2013-03-28 13:40:50 -0400

    Fixed bug

--- app/models/image.rb
+++ app/models/image.rb
@@ -67,7 +67,7 @@ class Image < ActiveRecord::Base
     fg = fg_color.to_weka(:rgb)
     bg = bg_color.to_weka(:rgb)
 
-    3.7329 * (fg[0] - bg[0])**2 + -16.8542 * (fg[1] - bg[1])**2 + 6.9221 * (fg[1] - bg[1]).abs +  0.6456 *  (fg[2] - bg[2]).abs - 35.0349
+    3.7329 * (fg[0] - bg[0])**2/1000 + -16.8542 * (fg[1] - bg[1])**2/1000 + 6.9221 * (fg[1] - bg[1]).abs +  0.6456 *  (fg[2] - bg[2]).abs - 35.0349
   end
 
   def get_w3c
